52db4179f29df442f82849ceb7b0455027830f05
mihz0 <59415467+mihz0@users.noreply.github.com>
2020-11-21 00:57:00 +0100

    Update shuffle.js

--- bot/commands/shuffle.js
+++ bot/commands/shuffle.js
@@ -6,6 +6,7 @@ module.exports = {
         const { channel, channelID } = m.member.voice;
         const player = m.client.manager.players.get(m.guild.id);
         if (!player || !player.queue.current) return m.channel.send("I must be playing music first.");
+        if (!player.queue.length < 1) return m.channel.send("The music queue is empty.");
         if (!channel) return m.channel.send("You must have an active voice connection.");
         if (m.guild.me.voice.channel && m.guild.me.voice.channelID !== channelID) return m.channel.send("I'm already being used in an other voice channel.");
 
