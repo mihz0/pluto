11288b701c3a51a15aa4bb3d767d0735db08f7ef
mihz0 <59415467+mihz0@users.noreply.github.com>
2020-11-20 23:37:53 +0100

    Update leave.js

--- bot/commands/leave.js
+++ bot/commands/leave.js
@@ -4,7 +4,7 @@ module.exports = {
 
         const player = m.client.manager.players.get(m.guild.id);
         const { channel, channelID } = m.member.voice;
-        if (!player || !player.queue.current) return m.channel.send("I'm not connected to any voice channel here.");
+        if (!player) return m.channel.send("I'm not connected to any voice channel here.");
         if (!channel) return m.channel.send("You must have an active voice connection.");
         if (m.guild.me.voice.channel && m.guild.me.voice.channelID !== channelID) return m.channel.send("I'm already being used in an other voice channel.");
         
